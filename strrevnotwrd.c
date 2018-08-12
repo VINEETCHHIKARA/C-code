750f029aaffb2e5c13fba3ce8604ffa3bee16ba4
VINEET CHAUDHARY <vineet.1614077@kiet.edu>
2018-08-12 20:35:00 +0530

    String reverse but not word

--- strrevnotwrd.c
+++ strrevnotwrd.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 void main(){
-    char str[20],temp[20];
-    int i,j,k,t;
-    printf("Enter the santance :");
+    char str[30];
+    int i;
+    printf("Enter the sentence :");
     gets(str);
     for(i=strlen(str)-1;i>=0;i--){
        if (str[i] == ' ') {
